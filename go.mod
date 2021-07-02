824a06467b64bc157ed028b0b183140755bf3072
wy <wy@wy.com>
2021-07-02 11:08:46 +0800

    go fmt

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rockiecn/interact
 
 go 1.16
+
+require github.com/ethereum/go-ethereum v1.10.4 // indirect
--- main.go
+++ main.go
@@ -27,131 +27,130 @@ func main() {
 	var cmd string
 
 	fmt.Println("intput cmd, 1 to retrieve data, 2 to store data, 3 to deploy contract")
-	fmt.Scanf("%s",&cmd)
+	fmt.Scanf("%s", &cmd)
 
 	switch cmd {
 	case "1":
 		CallRetrieve()
 
-	case "2": 
+	case "2":
 		fmt.Println("input data:")
 		var idata int64
 
-		fmt.Scanf("%d",&idata)
-		fmt.Printf("your input: %d\n",idata)
+		fmt.Scanf("%d", &idata)
+		fmt.Printf("your input: %d\n", idata)
 		data := big.NewInt(idata)
 		CallStore(data)
-		
+
 	case "3":
 		fmt.Println("deploying")
 		CallDeploy()
 	}
-	
+
 }
 
 //
 func CallRetrieve() error {
-    //cli, err := ethclient.Dial("http://localhost:8545")
+	//cli, err := ethclient.Dial("http://localhost:8545")
 	//cli, err := ethclient.Dial("../myGeth/data/geth.ipc")
-	fmt.Println("HOST: ",HOST)
-	cli ,err := getClient(HOST)
+	fmt.Println("HOST: ", HOST)
+	cli, err := getClient(HOST)
 	if err != nil {
-			fmt.Println("failed to dial geth", err)
-			return err
-	}
-	defer cli.Close()
-	
-    //
-    storageInstance, err := storage.NewStorage(common.HexToAddress("0x1714888Ede3a57b72781ff876dcb491cA3b4f744"), cli)
-	if err != nil {
-		fmt.Println("NewStorage err: ",err)
-	} else {
-		fmt.Println("NewStorage success: ",storageInstance)
-	}
-	
-    //
-    res, err := storageInstance.Retrieve(nil)
-	if err != nil {
-        fmt.Println("retrieve err:", err)
-        return err
-    }
-	
-    fmt.Println("result is", res)
-    return err
-}
-
-//
-func CallStore(n *big.Int) error {
-	fmt.Println("HOST: ",HOST)
-	cli ,err := getClient(HOST)
-	if err != nil {
-			fmt.Println("failed to dial geth", err)
-			return err
+		fmt.Println("failed to dial geth", err)
+		return err
 	}
 	defer cli.Close()
 
 	//
-    sk, err :=  crypto.HexToECDSA("cb61e1519b560d994e4361b34c181656d916beb68513cff06c37eb7d258bf93d")
-    if err != nil {
-	    fmt.Println("HexToECDSA err: ",err)
-    } else {
-		fmt.Println("get sk: ",sk)
+	storageInstance, err := storage.NewStorage(common.HexToAddress("0x1714888Ede3a57b72781ff876dcb491cA3b4f744"), cli)
+	if err != nil {
+		fmt.Println("NewStorage err: ", err)
+	} else {
+		fmt.Println("NewStorage success: ", storageInstance)
 	}
-    auth := bind.NewKeyedTransactor(sk)
+
+	//
+	res, err := storageInstance.Retrieve(nil)
+	if err != nil {
+		fmt.Println("retrieve err:", err)
+		return err
+	}
+
+	fmt.Println("result is", res)
+	return err
+}
+
+//
+func CallStore(n *big.Int) error {
+	fmt.Println("HOST: ", HOST)
+	cli, err := getClient(HOST)
+	if err != nil {
+		fmt.Println("failed to dial geth", err)
+		return err
+	}
+	defer cli.Close()
+
+	//
+	sk, err := crypto.HexToECDSA("cb61e1519b560d994e4361b34c181656d916beb68513cff06c37eb7d258bf93d")
+	if err != nil {
+		fmt.Println("HexToECDSA err: ", err)
+	} else {
+		fmt.Println("get sk: ", sk)
+	}
+	auth := bind.NewKeyedTransactor(sk)
 	if err != nil {
 		fmt.Println("auth err:", err)
 	} else {
-		fmt.Printf("auth success: %v\n",auth)
+		fmt.Printf("auth success: %v\n", auth)
 	}
 
-    //
-    storageInstance, err := storage.NewStorage(common.HexToAddress("0x1714888Ede3a57b72781ff876dcb491cA3b4f744"), cli)
+	//
+	storageInstance, err := storage.NewStorage(common.HexToAddress("0x1714888Ede3a57b72781ff876dcb491cA3b4f744"), cli)
 	if err != nil {
-		fmt.Println("NewStorage err: ",err)
+		fmt.Println("NewStorage err: ", err)
 		return err
 	} else {
-		fmt.Println("NewStorage success: ",storageInstance)
+		fmt.Println("NewStorage success: ", storageInstance)
 	}
-	
-    //
-    fmt.Printf("n = %s\n",n.String())
-	
-	tx, err:= storageInstance.Store(auth,n)
-    if err != nil {
-	    fmt.Println("tx failed :", err)
-        return err
-    }
 
-    fmt.Println("tx:", tx)
-	
-    return err
+	//
+	fmt.Printf("n = %s\n", n.String())
+
+	tx, err := storageInstance.Store(auth, n)
+	if err != nil {
+		fmt.Println("tx failed :", err)
+		return err
+	}
+
+	fmt.Println("tx:", tx)
+
+	return err
 }
-
 
 //
 func CallDeploy() (common.Address, error) {
 	var storageAddr common.Address
 
-	fmt.Println("HOST: ",HOST)
-	cli ,err := getClient(HOST)
+	fmt.Println("HOST: ", HOST)
+	cli, err := getClient(HOST)
 	if err != nil {
-			fmt.Println("failed to dial geth", err)
-			return storageAddr, err
+		fmt.Println("failed to dial geth", err)
+		return storageAddr, err
 	}
 	defer cli.Close()
 
-    sk, err :=  crypto.HexToECDSA("cb61e1519b560d994e4361b34c181656d916beb68513cff06c37eb7d258bf93d")
-    if err != nil {
-	    fmt.Println("HexToECDSA err: ",err)
-    } else {
-		fmt.Println("get sk: ",sk)
+	sk, err := crypto.HexToECDSA("cb61e1519b560d994e4361b34c181656d916beb68513cff06c37eb7d258bf93d")
+	if err != nil {
+		fmt.Println("HexToECDSA err: ", err)
+	} else {
+		fmt.Println("get sk: ", sk)
 	}
 
-    auth := bind.NewKeyedTransactor(sk)
+	auth := bind.NewKeyedTransactor(sk)
 	if err != nil {
 		fmt.Println("auth err:", err)
 	} else {
-		fmt.Printf("auth success: %v\n",auth)
+		fmt.Printf("auth success: %v\n", auth)
 	}
 
 	storageAddr, _, _, err = storage.DeployStorage(auth, cli)
@@ -166,12 +165,12 @@ func CallDeploy() (common.Address, error) {
 
 //
 func getClient(endPoint string) (*ethclient.Client, error) {
- rpcClient, err := rpc.Dial(endPoint)
- if err != nil {
-  log.Println("rpc.Dial err:", err)
-  return nil, err
- }
- 
- conn := ethclient.NewClient(rpcClient)
- return conn, nil
+	rpcClient, err := rpc.Dial(endPoint)
+	if err != nil {
+		log.Println("rpc.Dial err:", err)
+		return nil, err
+	}
+
+	conn := ethclient.NewClient(rpcClient)
+	return conn, nil
 }
