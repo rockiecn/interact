e5b0262158e240b2829cbac8308fe39e304949c3
wy <wy@wy.com>
2021-08-06 11:05:29 +0800

    add cash

---
+++ callcash/callcash.go
@@ -0,0 +1,130 @@
+package callcash
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"github.com/rockiecn/interact/cash"
+	"github.com/rockiecn/interact/clientops"
+)
+
+const HOST = "http://localhost:8545"
+
+//
+func CallApplyCheque(n *big.Int) error {
+	fmt.Println("HOST: ", HOST)
+	cli, err := clientops.GetClient(HOST)
+	if err != nil {
+		fmt.Println("failed to dial geth", err)
+		return err
+	}
+	defer cli.Close()
+
+	hexSk := "cb61e1519b560d994e4361b34c181656d916beb68513cff06c37eb7d258bf93d"
+	auth, err := clientops.MakeAuth(hexSk, nil, nil, big.NewInt(1000), 3000000)
+	if err != nil {
+		return err
+	}
+
+	//
+	cashInstance, err := cash.NewCash(common.HexToAddress("0x52D898a941C0b3d12C19b45008c210965644c7D5"), cli)
+	if err != nil {
+		fmt.Println("NewStorage err: ", err)
+		return err
+	} else {
+		fmt.Println("NewStorage success: ", cashInstance)
+	}
+
+	//
+	fmt.Printf("n = %s\n", n.String())
+
+	// address to receive money
+	toAddress := common.HexToAddress("0xb213d01542d129806d664248a380db8b12059061")
+	// transfer 1 eth to receiver
+	tx, err := cashInstance.ApplyCheque(auth, toAddress, big.NewInt(1000000000000000000))
+	if err != nil {
+		fmt.Println("tx failed :", err)
+		return err
+	}
+
+	fmt.Println("tx:", tx)
+
+	return err
+}
+
+//
+func CallDeploy() (common.Address, error) {
+	var cashAddr common.Address
+
+	fmt.Println("HOST: ", HOST)
+	client, err := clientops.GetClient(HOST)
+	if err != nil {
+		fmt.Println("failed to dial geth", err)
+		return cashAddr, err
+	}
+	defer client.Close()
+
+	// get sk
+	sk, err := crypto.HexToECDSA("cb61e1519b560d994e4361b34c181656d916beb68513cff06c37eb7d258bf93d")
+	if err != nil {
+		fmt.Println("HexToECDSA err: ", err)
+	} else {
+		fmt.Println("get sk: ", sk)
+	}
+
+	//
+	publicKey := sk.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	//
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
+	auth, err := bind.NewKeyedTransactorWithChainID(sk, big.NewInt(1337))
+	if err != nil {
+		log.Panic("NewKeyedTransactorWithChainID err:", err)
+	}
+
+	auth.Nonce = big.NewInt(int64(nonce))
+	// string to bigint
+	bn := new(big.Int)
+	bn, ok1 := bn.SetString("100000000000000000000", 10) // deploy 100 eth
+	if !ok1 {
+		fmt.Println("SetString: error")
+		panic("SetString error")
+	}
+	auth.Value = bn                // deploy 100 eth
+	auth.GasLimit = uint64(230000) // in units
+	auth.GasPrice = gasPrice
+
+	fmt.Printf("auth success: %v\n", auth)
+
+	cashAddr, _, _, err = cash.DeployCash(auth, client)
+	if err != nil {
+		log.Println("deployCashErr:", err)
+		return cashAddr, err
+	}
+	log.Println("storageAddr:", cashAddr.String())
+	return cashAddr, nil
+
+}
---
+++ cash/cash.go
@@ -0,0 +1,363 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package cash
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// CashABI is the input ABI used to generate the binding from.
+const CashABI = "[{\"inputs\":[],\"stateMutability\":\"payable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"oldOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnerSet\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"addresspayable\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"apply_cheque\",\"outputs\":[],\"stateMutability\":\"payable\",\"type\":\"function\"}]"
+
+// CashBin is the compiled bytecode used for deploying new contracts.
+var CashBin = "0x6080604052336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff16021790555060008054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16600073ffffffffffffffffffffffffffffffffffffffff167f342827c97908e5e2f71151c08502a66d44b6f758e3ac2f1de95f02eb95f0a73560405160405180910390a3610199806100ce6000396000f3fe60806040526004361061001e5760003560e01c8063b37aafaf14610023575b600080fd5b61003d600480360381019061003891906100b4565b61003f565b005b8173ffffffffffffffffffffffffffffffffffffffff166108fc829081150290604051600060405180830381858888f19350505050158015610085573d6000803e3d6000fd5b505050565b60008135905061009981610135565b92915050565b6000813590506100ae8161014c565b92915050565b600080604083850312156100cb576100ca610130565b5b60006100d98582860161008a565b92505060206100ea8582860161009f565b9150509250929050565b60006100ff82610106565b9050919050565b600073ffffffffffffffffffffffffffffffffffffffff82169050919050565b6000819050919050565b600080fd5b61013e816100f4565b811461014957600080fd5b50565b61015581610126565b811461016057600080fd5b5056fea2646970667358221220d48a7cbda7b47b6b92bc4df926f0c9e50653abccf09ea81a89d624ff8fca010764736f6c63430008060033"
+
+// DeployCash deploys a new Ethereum contract, binding an instance of Cash to it.
+func DeployCash(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Cash, error) {
+	parsed, err := abi.JSON(strings.NewReader(CashABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(CashBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Cash{CashCaller: CashCaller{contract: contract}, CashTransactor: CashTransactor{contract: contract}, CashFilterer: CashFilterer{contract: contract}}, nil
+}
+
+// Cash is an auto generated Go binding around an Ethereum contract.
+type Cash struct {
+	CashCaller     // Read-only binding to the contract
+	CashTransactor // Write-only binding to the contract
+	CashFilterer   // Log filterer for contract events
+}
+
+// CashCaller is an auto generated read-only Go binding around an Ethereum contract.
+type CashCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// CashTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type CashTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// CashFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type CashFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// CashSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type CashSession struct {
+	Contract     *Cash             // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// CashCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type CashCallerSession struct {
+	Contract *CashCaller   // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// CashTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type CashTransactorSession struct {
+	Contract     *CashTransactor   // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// CashRaw is an auto generated low-level Go binding around an Ethereum contract.
+type CashRaw struct {
+	Contract *Cash // Generic contract binding to access the raw methods on
+}
+
+// CashCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type CashCallerRaw struct {
+	Contract *CashCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// CashTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type CashTransactorRaw struct {
+	Contract *CashTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewCash creates a new instance of Cash, bound to a specific deployed contract.
+func NewCash(address common.Address, backend bind.ContractBackend) (*Cash, error) {
+	contract, err := bindCash(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Cash{CashCaller: CashCaller{contract: contract}, CashTransactor: CashTransactor{contract: contract}, CashFilterer: CashFilterer{contract: contract}}, nil
+}
+
+// NewCashCaller creates a new read-only instance of Cash, bound to a specific deployed contract.
+func NewCashCaller(address common.Address, caller bind.ContractCaller) (*CashCaller, error) {
+	contract, err := bindCash(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &CashCaller{contract: contract}, nil
+}
+
+// NewCashTransactor creates a new write-only instance of Cash, bound to a specific deployed contract.
+func NewCashTransactor(address common.Address, transactor bind.ContractTransactor) (*CashTransactor, error) {
+	contract, err := bindCash(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &CashTransactor{contract: contract}, nil
+}
+
+// NewCashFilterer creates a new log filterer instance of Cash, bound to a specific deployed contract.
+func NewCashFilterer(address common.Address, filterer bind.ContractFilterer) (*CashFilterer, error) {
+	contract, err := bindCash(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &CashFilterer{contract: contract}, nil
+}
+
+// bindCash binds a generic wrapper to an already deployed contract.
+func bindCash(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(CashABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Cash *CashRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Cash.Contract.CashCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Cash *CashRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Cash.Contract.CashTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Cash *CashRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Cash.Contract.CashTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Cash *CashCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Cash.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Cash *CashTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Cash.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Cash *CashTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Cash.Contract.contract.Transact(opts, method, params...)
+}
+
+// ApplyCheque is a paid mutator transaction binding the contract method 0xb37aafaf.
+//
+// Solidity: function apply_cheque(address to, uint256 amount) payable returns()
+func (_Cash *CashTransactor) ApplyCheque(opts *bind.TransactOpts, to common.Address, amount *big.Int) (*types.Transaction, error) {
+	return _Cash.contract.Transact(opts, "apply_cheque", to, amount)
+}
+
+// ApplyCheque is a paid mutator transaction binding the contract method 0xb37aafaf.
+//
+// Solidity: function apply_cheque(address to, uint256 amount) payable returns()
+func (_Cash *CashSession) ApplyCheque(to common.Address, amount *big.Int) (*types.Transaction, error) {
+	return _Cash.Contract.ApplyCheque(&_Cash.TransactOpts, to, amount)
+}
+
+// ApplyCheque is a paid mutator transaction binding the contract method 0xb37aafaf.
+//
+// Solidity: function apply_cheque(address to, uint256 amount) payable returns()
+func (_Cash *CashTransactorSession) ApplyCheque(to common.Address, amount *big.Int) (*types.Transaction, error) {
+	return _Cash.Contract.ApplyCheque(&_Cash.TransactOpts, to, amount)
+}
+
+// CashOwnerSetIterator is returned from FilterOwnerSet and is used to iterate over the raw logs and unpacked data for OwnerSet events raised by the Cash contract.
+type CashOwnerSetIterator struct {
+	Event *CashOwnerSet // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *CashOwnerSetIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(CashOwnerSet)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(CashOwnerSet)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *CashOwnerSetIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *CashOwnerSetIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// CashOwnerSet represents a OwnerSet event raised by the Cash contract.
+type CashOwnerSet struct {
+	OldOwner common.Address
+	NewOwner common.Address
+	Raw      types.Log // Blockchain specific contextual infos
+}
+
+// FilterOwnerSet is a free log retrieval operation binding the contract event 0x342827c97908e5e2f71151c08502a66d44b6f758e3ac2f1de95f02eb95f0a735.
+//
+// Solidity: event OwnerSet(address indexed oldOwner, address indexed newOwner)
+func (_Cash *CashFilterer) FilterOwnerSet(opts *bind.FilterOpts, oldOwner []common.Address, newOwner []common.Address) (*CashOwnerSetIterator, error) {
+
+	var oldOwnerRule []interface{}
+	for _, oldOwnerItem := range oldOwner {
+		oldOwnerRule = append(oldOwnerRule, oldOwnerItem)
+	}
+	var newOwnerRule []interface{}
+	for _, newOwnerItem := range newOwner {
+		newOwnerRule = append(newOwnerRule, newOwnerItem)
+	}
+
+	logs, sub, err := _Cash.contract.FilterLogs(opts, "OwnerSet", oldOwnerRule, newOwnerRule)
+	if err != nil {
+		return nil, err
+	}
+	return &CashOwnerSetIterator{contract: _Cash.contract, event: "OwnerSet", logs: logs, sub: sub}, nil
+}
+
+// WatchOwnerSet is a free log subscription operation binding the contract event 0x342827c97908e5e2f71151c08502a66d44b6f758e3ac2f1de95f02eb95f0a735.
+//
+// Solidity: event OwnerSet(address indexed oldOwner, address indexed newOwner)
+func (_Cash *CashFilterer) WatchOwnerSet(opts *bind.WatchOpts, sink chan<- *CashOwnerSet, oldOwner []common.Address, newOwner []common.Address) (event.Subscription, error) {
+
+	var oldOwnerRule []interface{}
+	for _, oldOwnerItem := range oldOwner {
+		oldOwnerRule = append(oldOwnerRule, oldOwnerItem)
+	}
+	var newOwnerRule []interface{}
+	for _, newOwnerItem := range newOwner {
+		newOwnerRule = append(newOwnerRule, newOwnerItem)
+	}
+
+	logs, sub, err := _Cash.contract.WatchLogs(opts, "OwnerSet", oldOwnerRule, newOwnerRule)
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(CashOwnerSet)
+				if err := _Cash.contract.UnpackLog(event, "OwnerSet", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
+
+// ParseOwnerSet is a log parse operation binding the contract event 0x342827c97908e5e2f71151c08502a66d44b6f758e3ac2f1de95f02eb95f0a735.
+//
+// Solidity: event OwnerSet(address indexed oldOwner, address indexed newOwner)
+func (_Cash *CashFilterer) ParseOwnerSet(log types.Log) (*CashOwnerSet, error) {
+	event := new(CashOwnerSet)
+	if err := _Cash.contract.UnpackLog(event, "OwnerSet", log); err != nil {
+		return nil, err
+	}
+	event.Raw = log
+	return event, nil
+}
